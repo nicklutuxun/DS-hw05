db8f8b7b17b087c4f38cd755b69136ab8e6f3ec8
Nick Lu <tlu32@jhu.edu>
2022-03-17 18:48:42 -0400

    Add tests

--- src/test/java/hw5/DequeTest.java
+++ src/test/java/hw5/DequeTest.java
@@ -1,11 +1,11 @@
 package hw5;
 
+import exceptions.EmptyException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public abstract class DequeTest {
 
@@ -26,5 +26,92 @@ public abstract class DequeTest {
   }
 
   // TODO Add more tests
-
+  @Test
+  @DisplayName("front() throws EmptyException if empty.")
+  public void testFrontThrowsExceptionIfEmpty() {
+    try {
+      deque.front();
+      fail("Test failed!");
+    } catch (EmptyException ex) {
+      System.out.println("Test passed!");
+    }
+  }
+  
+  @Test
+  @DisplayName("back() throws EmptyException if empty.")
+  public void testBackThrowsExceptionIfEmpty() {
+    try {
+      deque.back();
+      fail("Test failed!");
+    } catch (EmptyException ex) {
+      System.out.println("Test passed!");
+    }
+  }
+  
+  @Test
+  @DisplayName("insertFront() correctly insert at front.")
+  public void insertFrontCorrectlyInsertAtFront() {
+    deque.insertFront("a");
+    deque.insertFront("b");
+    deque.insertFront("c");
+    deque.insertFront("d");
+    assertEquals("d", deque.front());
+  }
+  
+  @Test
+  @DisplayName("insertBack() correctly insert at back.")
+  public void insertBackCorrectlyInsertAtFront() {
+    deque.insertBack("a");
+    deque.insertBack("b");
+    deque.insertBack("c");
+    deque.insertBack("d");
+    assertEquals("d", deque.back());
+  }
+  
+  @Test
+  @DisplayName("removeFront() throws EmptyException if empty")
+  public void removeFrontThrowsEmptyExceptionIfEmpty() {
+    try {
+      deque.removeFront();
+      fail("Test failed!");
+    } catch (EmptyException ex) {
+      System.out.println("Test passed!");
+    }
+  }
+  
+  @Test
+  @DisplayName("removeBack() throws EmptyException if empty")
+  public void removeBackThrowsEmptyExceptionIfEmpty() {
+    try {
+      deque.removeBack();
+      fail("Test failed!");
+    } catch (EmptyException ex) {
+      System.out.println("Test passed!");
+    }
+  }
+  
+  @Test
+  @DisplayName("length correctly returns number of elements in the deque")
+  public void lengthCorrectlyReturnsNumberOfElement() {
+    deque.insertBack("a");
+    deque.insertBack("b");
+    deque.insertBack("c");
+    deque.insertBack("d");
+    assertEquals(4, deque.length());
+    deque.removeBack();
+    deque.removeBack();
+    deque.removeBack();
+    deque.removeBack();
+    assertEquals(0, deque.length());
+    deque.insertFront("a");
+    deque.insertFront("b");
+    deque.insertFront("c");
+    deque.insertFront("d");
+    assertEquals(4, deque.length());
+    deque.removeFront();
+    deque.removeFront();
+    deque.removeFront();
+    deque.removeFront();
+    assertEquals(0, deque.length());
+  }
 }
