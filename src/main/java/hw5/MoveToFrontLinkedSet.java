42a95481f8b559f386060b5ee3b394e1d89dd5fa
Nick Lu <tlu32@jhu.edu>
2022-03-18 01:07:21 -0400

    Implement MoveToFrontLinkedSet

--- src/main/java/hw5/MoveToFrontLinkedSet.java
+++ src/main/java/hw5/MoveToFrontLinkedSet.java
@@ -9,5 +9,34 @@ public class MoveToFrontLinkedSet<T> extends LinkedSet<T> {
 
   // TODO: incorporate move-to-front heuristic each time a value is accessed.
   //  Override the relevant method(s) from LinkedSet.
-
+  
+  
+  @Override
+  protected Node<T> find(T t) {
+    for (Node<T> n = head; n != null; n = n.next) {
+      if (n.data.equals(t)) {
+        if (n == head) {
+          return head;
+        }
+        // Remove target
+        remove(n);
+        // Prepend to linked list
+        return prepend(t);
+      }
+    }
+    return null;
+  }
+  
+  private Node<T> prepend(T t) {
+    if (head == null) {
+      head = new Node<>(t);
+      tail = head;
+    }
+    Node<T> newNode = new Node<>(t);
+    head.prev = newNode;
+    newNode.next = head;
+    head = newNode;
+    return newNode;
+  }
+  
 }
