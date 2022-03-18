ce6c67df97bb1da01af89998be0c12256928320c
Nick Lu <tlu32@jhu.edu>
2022-03-18 01:17:51 -0400

    Implement TransposeArraySet

--- src/main/java/hw5/TransposeArraySet.java
+++ src/main/java/hw5/TransposeArraySet.java
@@ -9,5 +9,25 @@ public class TransposeArraySet<T> extends ArraySet<T> {
 
   // TODO: incorporate the transpose-sequential-search heuristic
   //  each time a value is accessed. Override the relevant method(s) from ArraySet.
-
+  
+  
+  @Override
+  protected int find(T t) {
+    for (int i = 0; i < numElements; i++) {
+      if (t.equals(data[i])) {
+        if (i == 0) {
+          return i;
+        }
+        return swapWithPredecessor(i);
+      }
+    }
+    return -1;
+  }
+  
+  private int swapWithPredecessor(int i) {
+    T temp = data[i];
+    data[i] = data[i - 1];
+    data[i - 1] = temp;
+    return i - 1;
+  }
 }
