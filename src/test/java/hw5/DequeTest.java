d4e5989dcaaf14773f23500af776ec12b9e22d70
Nick Lu <tlu32@jhu.edu>
2022-03-18 00:26:56 -0400

    Make tests more comprehensive

--- src/test/java/hw5/DequeTest.java
+++ src/test/java/hw5/DequeTest.java
@@ -8,23 +8,23 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public abstract class DequeTest {
-
+  
   private Deque<String> deque;
-
+  
   @BeforeEach
   public void setUp() {
     this.deque = createDeque();
   }
-
+  
   protected abstract Deque<String> createDeque();
-
+  
   @Test
   @DisplayName("Deque is empty after construction.")
   public void testConstructor() {
     assertTrue(deque.empty());
     assertEquals(0, deque.length());
   }
-
+  
   // TODO Add more tests
   @Test
   @DisplayName("front() throws EmptyException if empty.")
@@ -50,27 +50,25 @@ public abstract class DequeTest {
   
   @Test
   @DisplayName("insertFront() correctly insert at front.")
-  public void insertFrontCorrectlyInsertAtFront() {
-    deque.insertFront("a");
-    deque.insertFront("b");
-    deque.insertFront("c");
-    deque.insertFront("d");
-    assertEquals("d", deque.front());
+  public void testInsertFrontCorrectlyInsertAtFront() {
+    for (int i = 0; i < 200; i++) {
+      deque.insertFront(Integer.toString(i));
+      assertEquals(Integer.toString(i), deque.front());
+    }
   }
   
   @Test
   @DisplayName("insertBack() correctly insert at back.")
-  public void insertBackCorrectlyInsertAtBack() {
-    deque.insertBack("a");
-    deque.insertBack("b");
-    deque.insertBack("c");
-    deque.insertBack("d");
-    assertEquals("d", deque.back());
+  public void testInsertBackCorrectlyInsertAtBack() {
+    for (int i = 0; i < 200; i++) {
+      deque.insertBack(Integer.toString(i));
+      assertEquals(Integer.toString(i), deque.back());
+    }
   }
   
   @Test
   @DisplayName("removeFront() throws EmptyException if empty")
-  public void removeFrontThrowsEmptyExceptionIfEmpty() {
+  public void testRemoveFrontThrowsEmptyExceptionIfEmpty() {
     try {
       deque.removeFront();
       fail("Test failed!");
@@ -81,7 +79,7 @@ public abstract class DequeTest {
   
   @Test
   @DisplayName("removeBack() throws EmptyException if empty")
-  public void removeBackThrowsEmptyExceptionIfEmpty() {
+  public void testRemoveBackThrowsEmptyExceptionIfEmpty() {
     try {
       deque.removeBack();
       fail("Test failed!");
@@ -91,43 +89,50 @@ public abstract class DequeTest {
   }
   
   @Test
-  @DisplayName("length correctly returns number of elements in the deque")
-  public void lengthCorrectlyReturnsNumberOfElement() {
-    deque.insertBack("a");
-    deque.insertBack("b");
-    deque.insertBack("c");
-    deque.insertBack("d");
-    assertEquals(4, deque.length());
-    deque.removeBack();
-    deque.removeBack();
-    deque.removeBack();
-    deque.removeBack();
-    assertEquals(0, deque.length());
-    deque.insertFront("a");
-    deque.insertFront("b");
-    deque.insertFront("c");
-    deque.insertFront("d");
-    assertEquals(4, deque.length());
-    deque.removeFront();
-    deque.removeFront();
-    deque.removeFront();
-    deque.removeFront();
+  @DisplayName("length correctly returns number of elements in the deque with insertFront()")
+  public void testCorrectLengthInsertFront() {
+    for (int i = 0; i < 200; i++) {
+      deque.insertFront(Integer.toString(i));
+    }
+    assertEquals(200, deque.length());
+  }
+  
+  @Test
+  @DisplayName("length correctly returns number of elements in the deque with insertBack()")
+  public void testCorrectLengthInsertBack() {
+    for (int i = 0; i < 200; i++) {
+      deque.insertBack(Integer.toString(i));
+    }
+    assertEquals(200, deque.length());
+  }
+  
+  @Test
+  @DisplayName("Deque can behave like a front stack correctly")
+  public void testDequeBehaveLikeAFrontStack() {
+    for (int i = 0; i < 200; i++) {
+      deque.insertFront(Integer.toString(i));
+      assertEquals(Integer.toString(i), deque.front());
+    }
+    assertEquals(200, deque.length());
+    for (int i = 199; i >= 0; i--) {
+      assertEquals(Integer.toString(i), deque.front());
+      deque.removeFront();
+    }
     assertEquals(0, deque.length());
   }
   
   @Test
-  @DisplayName("Deque can behave like a stack correctly")
-  public void DequeCanBehaveLikeAStack() {
-    char ch = 'a';
-    for (int i = 0; i < 10; i++) {
-      deque.insertFront(Character.toString(ch));
-      ch++;
+  @DisplayName("Deque can behave like a back stack correctly")
+  public void testDequeBehaveLikeABackStack() {
+    
+    for (int i = 0; i < 200; i++) {
+      deque.insertBack(Integer.toString(i));
+      assertEquals(Integer.toString(i), deque.back());
     }
-    assertEquals(10, deque.length());
-    for (int i = 0; i < 10; i++) {
-      assertEquals(Character.toString(ch), deque.front());
-      deque.removeFront();
-      ch--;
+    assertEquals(200, deque.length());
+    for (int i = 199; i >= 0; i--) {
+      assertEquals(Integer.toString(i), deque.back());
+      deque.removeBack();
     }
     assertEquals(0, deque.length());
   }
